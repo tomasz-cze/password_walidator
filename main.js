550eb437eb6b858a2411d0e98f9e2b3f7d3d975b
unknown <tomasz.czechlewski@gmail.com>
2022-01-23 21:23:40 +0100

    simple password validator completed, checks if password contains number, letter, special sign and has min totaly 10 signs

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-const pass = document.querySelector('#password');
-const p = document.querySelector('.passinfo');
-const letters = /[a-z]/i;
-const numbers = /[0-9]/;
-const special = /[!@#$%^&*()]/;
-const minValue = 10;
+const pass = document.querySelector('#password')
+const p = document.querySelector('.passinfo')
+const letters = /[a-z]/i
+const numbers = /[0-9]/
+const special = /[!@#$%^&*()]/
+const minValue = 10
+
+const showMsg = () => {
+	if (pass.value.length >= minValue && pass.value.match(letters) && pass.value.match(numbers) && pass.value.match(special)) {
+				p.textContent = 'Twoje hasło jest idealne';
+                p.style.color = "lime";
+			} else if (pass.value.length >= minValue && pass.value.match(letters)  && pass.value.match(special)) {
+                p.textContent = 'brakuje cyfry';
+                p.style.color = "orange";
+            } else if (pass.value.length >= minValue && pass.value.match(numbers)  && pass.value.match(special)) {
+                p.textContent = 'brakuje litery';
+                p.style.color = "orange";
+            } else if (pass.value.length >= minValue && pass.value.match(letters)  && pass.value.match(numbers)) {
+                p.textContent = 'brakuje znaku specjalnego';
+                p.style.color = "orange";
+            } else if (pass.value.length < minValue) {
+                p.textContent = 'hasło za krótkie';
+            } else if (pass.value.length >= minValue) {
+                p.textContent = 'wpisz cyfrę, znak specjalny';
+            }
+        
+        }
+            
+        
+    
+pass.addEventListener('keyup', showMsg);
